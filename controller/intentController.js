6e5680200f987750dd67ddb023f127e0537a6a7a
Saurav Dutta <saurav@piktorlabs.com>
2020-10-14 20:34:39 +0530

    changes

--- controller/intentController.js
+++ controller/intentController.js
@@ -4,6 +4,21 @@
  * @author Saurav Dutta
  */
 
+const axios = require("axios");
+
+const getWeatherData = async query => {
+  axios
+    .get(
+      `https://api.weatherapi.com/v1/current.json?key=d64b4096e02d47bcacb111415201410&q=${query.parameters.any}`
+    )
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
 /**
  * @function handleIntent
  * @description Function to get token from google API
@@ -11,33 +26,37 @@
  * @param {object} res response from server
  */
 const handleIntent = async (req, res) => {
-    try {
-        console.log('req ', req.body);
-        console.log('req from client ', req.body.queryResult);
-        res.status(200).send({
-            "fulfillmentMessages": [
-                {
-                  "text": {
-                    "text": [
-                      "Text response from webhook"
-                    ]
-                  }
-                }
-            ]
-        });
-    } catch(ex) {
-        res.status(500).send({
-            "fulfillmentMessages": [
-                {
-                  "text": {
-                    "text": [
-                      "Text response from webhook"
-                    ]
-                  }
-                }
-            ]
-        })
+  try {
+    console.log("req from client ", req.body.queryResult);
+    let response = "response seems to take long time";
+    switch (req.body.queryResult.action) {
+      case "location":
+        response = await getWeatherData(req.body.queryResult);
+        break;
+      default:
+        response = "Something seems to be broken, I need more training";
+        break;
     }
-}
+    res.status(200).send({
+      fulfillmentMessages: [
+        {
+          text: {
+            text: [response]
+          }
+        }
+      ]
+    });
+  } catch (ex) {
+    res.status(500).send({
+      fulfillmentMessages: [
+        {
+          text: {
+            text: [response]
+          }
+        }
+      ]
+    });
+  }
+};
 
-module.exports = { handleIntent }
+module.exports = { handleIntent };
